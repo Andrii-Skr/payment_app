ea1e8b99ca9574998fc9e74aec58d1409a05f1fe
Andrii-Skr <goodarky@gmail.com>
2025-05-08 12:21:12 +0000

    some fix

--- prisma/schema/migrations/20250507123029_2/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "api_request_log" ADD COLUMN     "body_json" JSONB;
--- prisma/schema/migrations/20250507140740_3/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "bank_info" ADD COLUMN     "not_update" BOOLEAN NOT NULL DEFAULT false;
